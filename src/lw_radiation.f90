a4af221f6fd80cc71e31001209a68ade6638e003
AJFMAHFOUF <ajfmahfouf.meteo@gmail.com>
2023-12-13 10:12:50 +0100

    revisions to lw_radiation - z removed from routine call

--- src/lw_radiation.f90
+++ src/lw_radiation.f90
@@ -1,4 +1,4 @@
-subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
+subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,dtdt)
 !-------------------------------------------------------------------------
 !
 ! Compute simplified longwave radiative cooling (Sasamori, 1972)
@@ -30,14 +30,13 @@ subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
  integer,                 intent(in)  :: nlev
  real,                    intent(in)  :: ts, qvs, ps
  real, dimension(nlev),   intent(in)  :: tha, qva, pa, rho
- real, dimension(nlev+1), intent(in)  :: z
  real, dimension(nlev),   intent(out) :: dtdt
 
  real, dimension(nlev)   :: rhoh, ta, tah, qvah, dtdth
  real, dimension(nlev)   :: uh2o_d, uco2_d, uh2o_u, uco2_u      
- real, dimension(nlev+1) :: zpa      
- real                    :: zrhos, emis_up1, emis_up2, emis_dn1, emis_dn2, invcpdz
- real                    :: zt_top, zp0, zt0, zscale_h2o, zscale_co2 
+ real, dimension(nlev+1) :: zpa, zpah      
+ real                    :: zrhos, emis_up1, emis_up2, emis_dn1, emis_dn2, invcpdp
+ real                    :: zt_top, zp0, zt0, zscale_h2o, zscale_co2, zdelta_emis 
  integer                 :: jk, jk1, jk2
  logical                 :: l_cts, l_wvcont, l_simtend
 !
@@ -51,6 +50,12 @@ subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
 !
  zp0 = 1.0E5
  zt0 = 273.0
+!
+! Empirical correction of emissivity at model top 
+! 
+ zdelta_emis = 0.0 ! 3.0E-5 for ICRCCM
+!
+! Mean atmospheric variables at half-levels
 ! 
  do jk=1,nlev-1
   rhoh(jk) = 0.5*(rho(jk) + rho(jk+1))
@@ -70,11 +75,12 @@ subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
  do jk=1,nlev
   if (jk /= nlev) then
     tah(jk)  = 0.5*(ta(jk) + ta(jk+1))
-    qvah(jk) = 0.5*(qva(jk) + qva(jk+1))
+    qvah(jk) = 0.5*(qva(jk) + qva(jk+1)) 
   else
     tah(jk)  = 0.5*(ta(jk) + ts)
     qvah(jk) = 0.5*(qva(jk) + qvs)
   endif 
+  zpah(jk) = exp(0.5*(log(zpa(jk)) + log(zpa(jk+1))))
  enddo
 ! 
 ! Temperature at model top - set to zero with a full profile (not only PBL)
@@ -89,8 +95,8 @@ subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
  uco2_d(:) = 0.0
  do jk1=1,nlev
    do jk2=jk1,nlev 
-     zscale_h2o = (zpa(jk2)/zp0)**0.85*(zt0/tah(jk2))**0.5
-     zscale_co2 = (zpa(jk2)/zp0)**0.75*(zt0/tah(jk2))**0.0
+     zscale_h2o = (zpah(jk2)/zp0)**0.85*(zt0/tah(jk2))**0.5
+     zscale_co2 = (zpah(jk2)/zp0)**0.75*(zt0/tah(jk2))**0.0
      uh2o_d(jk1) = uh2o_d(jk1) + 0.1/grav*qvah(jk2)*zscale_h2o*(zpa(jk2+1) - zpa(jk2))
      uco2_d(jk1) = uco2_d(jk1) + 0.00612*zscale_co2*(zpa(jk2+1) - zpa(jk2))
    enddo  
@@ -102,8 +108,8 @@ subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
  uco2_u(:) = 0.0
  do jk1=nlev,1,-1
    do jk2=jk1,1,-1 
-     zscale_h2o = (zpa(jk2)/zp0)**0.85*(zt0/tah(jk2))**0.5
-     zscale_co2 = (zpa(jk2)/zp0)**0.75*(zt0/tah(jk2))**0.0
+     zscale_h2o = (zpah(jk2)/zp0)**0.85*(zt0/tah(jk2))**0.5
+     zscale_co2 = (zpah(jk2)/zp0)**0.75*(zt0/tah(jk2))**0.0
      uh2o_u(jk1) = uh2o_u(jk1) + 0.1/grav*qvah(jk2)*zscale_h2o*(zpa(jk2+1) - zpa(jk2))
      uco2_u(jk1) = uco2_u(jk1) + 0.00612*zscale_co2*(zpa(jk2+1) - zpa(jk2))
    enddo  
@@ -117,7 +123,7 @@ subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
    if (jk .ne. 1) then    
      emis_up1 = emis3_h2o(uh2o_u(jk-1)) + emis1_co2(uco2_u(jk-1))
    else
-     emis_up1 = 1.0*emis_up2
+     emis_up1 = 1.0*emis_up2 - zdelta_emis
    endif 
 ! 
    emis_dn1 = emis3_h2o(uh2o_d(jk)) + emis1_co2(uco2_d(jk))
@@ -127,11 +133,9 @@ subroutine lw_radiation(nlev,ts,qvs,ps,tha,qva,pa,rho,z,dtdt)
      emis_dn2 = 1.0*emis_dn1
    endif      
 !   
-   !invcpdz = -Stefan/(rhoh(jk)*Cp*(z(jk+1) - z(jk)))
-! Modified formulation to prevent from too low density values at high levels   
-   invcpdz = grav*Stefan/(Cp*(zpa(jk+1) - zpa(jk)))
-   dtdth(jk) = invcpdz*((tah(jk)**4 - ts**4)*(emis_dn2 - emis_dn1) + &
-            &        (zt_top**4 - tah(jk)**4)*(emis_up2 - emis_up1))          
+   invcpdp = grav*Stefan/(Cp*(zpa(jk+1) - zpa(jk)))
+   dtdth(jk) = invcpdp*((tah(jk)**4 - ts**4)*(emis_dn2 - emis_dn1) + &
+            &       (zt_top**4 - tah(jk)**4)*(emis_up2 - emis_up1))          
 !
 !  Cooling to space approximation
 !             
